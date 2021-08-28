e29e970e775f8c1a3ffcd9eaf0f9790efe8c004d
Mo-Chen-e308 <89691713+Mo-Chen-e308@users.noreply.github.com>
2021-08-28 20:31:36 +0800

    Add files via upload

---
+++ js/layers(雾/T.js
@@ -0,0 +1,95 @@
+addLayer("T", {
+    name: "TAT", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new ExpantaNum(0),
+    }},
+    color: "#cceee5",
+	requires: new ExpantaNum(10),
+    resource: "T点", // Name of prestige currency
+	baseResource: "TAT",
+	baseAmount() {return player.points}, 
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+	exponent: 0.55,
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new ExpantaNum(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new ExpantaNum(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)  QwQ:1也可以当第一排
+    //layerShown(){return player.v.total.gte(1)},
+    //clickables: {
+        //part1
+        //11: {
+        //    canClick(){return true},
+        //    display() {return `Update the game<br />You've updated ${Utimeformat(player.u.updtime)}.<br /><br />Now you are doing:${updtxt[player.u.doing]}`},
+        //    onClick(){player.u.doing = "upd"}
+        //},
+    //},
+    /*
+    upgrades: {
+        11: {
+            description: "next update is in 5 hours。",
+            cost(){return new OmegaNum(5)},
+            unlocked(){return true},
+            currencyDisplayName:"hours of update time"
+        },
+    },
+    */
+    /*
+    challenges: {
+        11: {
+            name: "AntiLooperrrr",
+            challengeDescription: "因为挑战出了bug，devU13被禁用了。刷新后的第一帧时间计数x100。",
+            canComplete(){return player.points.gte(1e10)},
+            goalDescription(){return format(ExpantaNum(1e10))+"点数"},
+            rewardDisplay(){return `你永远保留dev11的效果，同时“刷新后的第一帧时间计数x100。”被保留。`},
+            unlocked(){return hasUpgrade("dev",15)}
+        },
+    },
+    */
+
+    //inportant!!!
+    //update(diff){
+    //}
+	//let gain = new ExpantaNum(0.5)
+	//if(hasUpqrade('Q',11)) gain  = new ExpantaNum(0.6)
+	//return gain
+	upgrades:{
+				11:{
+				title:"加成？",
+				description:"根据Q点<br>加成QwQ获取",
+				cost:new ExpantaNum(3),
+				effect(){
+					let eff = player[this.layer].points.add(1).pow(0) 
+				if (hasUpgrade('Q',11)) eff = player[this.layer].points.add(1).pow(0.2265);
+				if (hasUpgrade('Q',12)) eff = player[this.layer].points.add(1).pow(0.3375);
+				if (hasUpgrade('Q',13)) eff = player[this.layer].points.add(1).pow(0.4485);
+					return eff
+					},
+				   effectDisplay(){return format(upgradeEffect(this.layer,this.id))+"x"}
+				},
+				12:{
+				title:"加成！",
+				description:"改善Q11的加成<br>注：这是有加成的<br>直接在Q11上显示",
+				cost:new ExpantaNum(16),
+				unlocked(){return hasUpgrade("Q",11)},
+				},
+				13:{
+				title:"起飞了？",
+				description:"再改善Q11的加成<br>注：同Q12",
+				cost:new ExpantaNum(48),
+				unlocked(){return hasUpgrade("Q",12)},
+			       },
+				14:{
+				title:"起飞了！",
+				description:"根据QwQ点倍增<br>获取Q点的数量",
+				cost:new ExpantaNum(96),
+				unlocked(){return hasUpgrade("Q",13)},
+			       },
+			},
+})
